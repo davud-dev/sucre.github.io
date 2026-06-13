00617fdb6a3cf4b8b594a4d659295276037384d0
davud-dev <cazamirloucas+calt@gmail.com>
2026-06-13 08:41:37 +0300

    Refactor lexer methods and improve syntax handling

--- lexer.java
+++ lexer.java
@@ -1,4 +1,12 @@
 public class code { 
+  //helper functions
+  static boolean digit(char x) {return x >= '0' && x <= '9'};
+  static boolean letter(char x) {return (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z')};
+  static boolean alpha(char x) {return digit(x) || letter(x)};
+  static void move() {pos += 1; column += 1};
+  static void move(int n) {pos += n; column += n};
+  static void token(String type, String value) {tokens.add(new String[]{type, value})};
+  //lexer
   static lexer(String source) {
     int pos = 0;
     int line = 1;
@@ -10,7 +18,7 @@ public class code {
       '[', "LBRACK",
       ']', "RBRACK",
       '<', "LAB",
-      '>': "RAB"
+      '>', "RAB",
       '|', "PIPE",
       ':', "COLON",
       ',', "COMMA",
@@ -35,7 +43,7 @@ public class code {
       Map.entry("binteger", "BIG_INTEGER_TYPE"),
       Map.entry("float", "FLOAT_TYPE"),
       Map.entry("boolean", "BOOLEAN_TYPE"),
-      Map.entry("any", "ANY_TYPE",
+      Map.entry("any", "ANY_TYPE"),
       Map.entry("empty", "VALUELESS_DECLARE"),
       Map.entry("equals", "EQ"),
       //conditions
@@ -48,39 +56,32 @@ public class code {
       Map.entry("do", "LOOP_START"),
       Map.entry("finish", "BLOCK_FINISH"),
     );
-    boolean digit(char x) {return x >= '0' && x <= '9'};
-    boolean letter(char x) {return (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z')};
-    boolean alpha(char x) {return digit(x) || letter(x)};
-    void move() {pos += 1; column += 1};
-    void move(int n) {pos += n; column += n};
-    void token(String type, String value) {tokens.add(new String[]{type, value})};
     while(pos < source.length()) {
       char current = source.charAt(pos);
       switch(current) {
-        case '"': case '\'':
+        case '"', '\'' -> {
           String value = "";
           move();
           while(source.charAt(pos) !== current) {value += source.charAt(pos); move()};
           token("STRING", value);
           move();
-          break;
-        case ' ': case '\r': case '\t':
-          move();
-          break;
-        case '\n':
-          line += 1; column = 1;
-          move();
-          break;
-        case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ->
+        };
+        case ' ', '\r', '\t' -> move();
+        case '\n' -> {line += 1; column = 1; move()};
+        case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
           String value2 = "";
           while(digit(source.charAt(pos))) {value2 += source.charAt(pos); move()};
           token("NUMBER", value2);
-          break;
-        default:
+        };
+        default -> {
           if(letter(current)) {
             String value3 = "";
             while(alpha(source.charAt(pos)) || source.charAt(pos) == '_') {value3 += source.charAt(pos); move()};
             String type = keywords.get(value3);
             if(type != null) {token(type + "_KEYWORD", value3)} else {token("IDENTIFIER", value3)};
+          };
+        };
       };
+    };
+  };
 };
